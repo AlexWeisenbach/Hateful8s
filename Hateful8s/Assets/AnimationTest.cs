a033219a3e384b553eb176be46ea86e6c73ed0b0
Alex w <weisea3@rpi.edu>
2016-11-05 14:50:53 -0400

    Animations for stuff

---
+++ Hateful8s/Assets/AnimationTest.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationTest : MonoBehaviour {
+	public	Animator anim;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("d"))
+		{
+			anim.SetBool("WalkF", true);
+			anim.SetBool("Neutral", false);
+		}
+		if(Input.GetKeyUp("d"))
+		{
+			anim.SetBool("WalkF", false);
+			anim.SetBool("Neutral", true);
+		}
+		if(Input.GetKey("a"))
+		{
+			anim.SetBool("WalkB", true);
+			anim.SetBool("Neutral", false);
+		}
+		if(Input.GetKeyUp("a"))
+		{
+			anim.SetBool("WalkB", false);
+			anim.SetBool("Neutral", true);
+		}
+		if(Input.GetKeyDown("space"))
+		{
+			anim.SetBool("Punching", true);
+		}
+		if(Input.GetKeyUp("space"))
+		{
+			anim.SetBool("Punching", false);
+		}
+		if(Input.GetKey("w"))
+		{
+			anim.SetTrigger("Jump");		
+		}
+	}
+}
